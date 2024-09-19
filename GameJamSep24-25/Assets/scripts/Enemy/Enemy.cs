c4189c5fa9860807680884ca8599572cb410c8db
Vanina-J <vanina.jacques@student.heaj.be>
2024-09-19 12:46:28 +0200

    Update Enemy.cs

--- GameJamSep24-25/Assets/scripts/Enemy/Enemy.cs
+++ GameJamSep24-25/Assets/scripts/Enemy/Enemy.cs
@@ -7,8 +7,9 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject m_target;
     [SerializeField] private float m_speed = 10f;
+    [SerializeField] private float m_currentSpeed = m_speed;
     [SerializeField] private float m_deathTimer = 2f; //Timer at which the enemy disappears, must be after the VFX and Animation are done (value is a placeholder)
-
+    [SerializeField] private float m_offsetDist = 1f; //distance at which the enemy will stop going towards the player
 
     private EEnemyStateMach m_state;
 
@@ -23,7 +24,6 @@ public class Enemy : MonoBehaviour
     {
         m_target = GameObject.FindWithTag("Player");
         m_state = EEnemyStateMach.Spawning;
-
     }
 
     void Update()
@@ -53,12 +53,32 @@ public class Enemy : MonoBehaviour
 
     private void Attack()
     {
-        Vector3.MoveTowards(transform.position, m_target.transform.position, m_speed * Time.deltaTime);
-
+        transform.LookAt(m_target.transform.position);
+       
         //Make attack at random timing
 
         //When countered : 
         //m_state = EEnemyStateMach.Dead
+
+        
+        float distance = Mathf.Abs(Vector3.Distance(transform.position, m_target.transform.position));
+
+        if (distance > m_offsetDist)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, m_target.transform.position, m_speed * Time.deltaTime);
+        }
+        else
+        {
+            m_currentSpeed = 0;
+        }
+
+        //placeholder script for attack pattern
+        //if (m_timer >= m_attackRate)
+        //{
+        //      insert attack anim and vfx
+        //    m_timer = 0f;
+        //}
+
     }
 
     private void Die()
