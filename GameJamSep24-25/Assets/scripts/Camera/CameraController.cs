54b9a3b86496f3dbb3f06a5ba9c06320bc54dd8f
MaxensBoui <bmaxens@gmail.com>
2024-09-19 12:41:48 +0200

    Modifying camera script

--- GameJamSep24-25/Assets/scripts/Camera/CameraController.cs
+++ GameJamSep24-25/Assets/scripts/Camera/CameraController.cs
@@ -6,9 +6,13 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject m_player;
     [SerializeField] private Vector3 m_positionOffset;
+    [SerializeField] private float m_smoothTime = 0.2f;
+    private Vector3 m_cameraTransform;
+    private Vector3 m_velocity = Vector3.zero;
   
     void Update()
     {
-        transform.position = m_positionOffset + m_player.transform.position;
+        m_cameraTransform = m_positionOffset + m_player.transform.position;
+        transform.position = Vector3.SmoothDamp(transform.position, m_cameraTransform,ref m_velocity, m_smoothTime);
     }
 }
